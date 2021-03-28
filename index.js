e89dbb088e27fab1982a5303b299d63a452c948a
bayu firnanda <bafirgenius@gmail.com>
2021-03-28 16:53:11 +0700

    A

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { PORT_ENV } = process.env;
 const express = require('express')
 const app = express()
-// const session = require("express-session");
+const session = require("express-session");
 require("./config/database");
 
 const routes = require('./routes');
@@ -11,11 +11,11 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(session({
-//     secret: "session",
-//     resave: false,
-//     saveUninitialized: true,
-// }))
+app.use(session({
+    secret: "session",
+    resave: false,
+    saveUninitialized: true,
+}))
 app.use(routes)
 
 app.listen(PORT_ENV, console.log(`app running on port ${PORT_ENV}`))
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const router = require("express").Router()
 const loginController = require("../controllers/login")
 const dataRouter = require("./data")
-// const authentication = require('../middlewares/session')
+const authentication = require('../middlewares/session')
 
 router.get("/", loginController.login)
 router.post("/", loginController.loginPost)
 
-// router.use(authentication);
+router.use(authentication);
 
 router.use("/", dataRouter)
 
